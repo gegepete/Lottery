84cc7902cd31489357f7c6fe2c4718a0d8e6a947
George <george.giurgiu@gmail.com>
2018-08-07 12:35:34 +0300

    all done

--- Lottery.java
+++ Lottery.java
@@ -1,4 +1,72 @@
 import java.util.*;
 public class Lottery {
+    public final static int COMBINATII = 3; // numarul de combinatii
+    public final static int ELEMENTE = 6; //numarul de elemente dintr-o combinatie
+    public final static int MAX_N = 16; // val limita a elementelor
+    private static Lottery singleton;
+    
+    private Map<Integer, List<Integer>> combinatii = new LinkedHashMap<>();
+    private Random random = new Random();
+    
+    private Lottery(){
+    }
+    
+    public static Lottery getInstance(){
+        if (singleton == null) {
+            singleton = new Lottery();
+        }
+        return singleton;
+    }
+    
+    public void genereaza(){
+        for (int i = 0; i < COMBINATII; ++i) {
+            List<Integer> combinatie = new ArrayList<>();
+            
+            for (int j = 0; j < ELEMENTE; ++j) {
+                combinatie.add(random.nextInt(MAX_N) + 1);
+            }
+            
+            combinatii.put(i, combinatie);
+        }
+    }
+    
+    public int calculeaza(List<Integer> numerePariate){
+        int punctaj = 0;
+        
+        genereaza();
+        
+        for (int i = 0; i < COMBINATII; ++i) {
+            List<Integer> combinatie = combinatii.get(i);
+            
+            for (int j = 0; j < combinatie.size(); ++j) {
+                int dif = Math.abs(numerePariate.get(j) - combinatie.get(j));
+                
+                switch (dif) {
+                    case 0 :
+                        punctaj += 100;
+                        break;
+                    case 1 :
+                        punctaj += 50;
+                        break;
+                    case 2 :
+                        punctaj += 25;
+                        break;
+                }
+            }
+        }
+        
+        // combinatii.clear();
+        
+        return punctaj;
+    }
+    
+    public void afiseaza(){
+        combinatii.forEach((k, v) -> {
+            for (Integer x : v) {
+                System.err.print(x + " ");
+            }
+            System.err.println();
+        });
+    }
     
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,36 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-    
+        Scanner scanner = new Scanner(System.in);
+        
+        String line;
+        String[] input;
+        
+        while (true) {
+            line = scanner.nextLine();
+            input = line.split("\\s+");
+            
+            switch (input[0].toUpperCase()) {
+                case "BET" :
+                    List<Integer> numerePariate = new ArrayList<>();
+                    for (int i = 0; i <= Lottery.ELEMENTE; i++) {
+                        int x = Integer.parseInt(input[i]);
+                        numerePariate.add(x);
+                    }
+                    int castig = Lottery.getInstance().calculeaza(numerePariate);
+                    
+                    System.out.println("Ai castigat " + castig);
+                    
+                    Lottery.getInstance().afiseaza();
+                    
+                    break;
+                case "EXIT" :
+                    System.exit(0);
+                    break;
+                default :
+                    System.err.println("Comanda invalida");
+            }
+        }
+            
     }
 }
